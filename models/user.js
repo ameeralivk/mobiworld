ec0f70f4392a4534314ab5596d2f3db6029788a1
ameer ali vk <ameervk1875@gamil.com>
2025-02-23 17:43:54 +0530

    commit1

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose')
-
-const userschema = new mongoose.Schema ({
+const mongoose = require('mongoose');
+const { search } = require('../app');
+const {Schema} = mongoose;
+const userschema = new Schema ({
     name: {
         type: String,
         required: true,
@@ -12,7 +13,69 @@ const userschema = new mongoose.Schema ({
     password:{
         type:String,
         required:true,
-    }
+    },
+    phone: {
+        type: String,
+        required:false,
+        unique:false,
+        sparse:true,
+        default:null,
+    },
+    googleId:{
+        type : String,
+        unique : true,
+    },
+    isBlocked:{
+        type : Boolean,
+        default : false,
+    },
+    isAdmin : {
+        type: Boolean,
+        default : false,
+    },
+    cart:[{
+        type:Schema.Types.ObjectId,
+        ref:"Cart",
+    }],
+    wallet:{
+        type:Number,
+        default:0,
+    },
+    wishlist:[{
+        type:Schema.Types.ObjectId,
+        ref: 'Wishlist'
+    }],
+    orderHistory:[{
+        type: Schema.Types.ObjectId,
+        ref:"Order"
+    }],
+    createdOn:{
+        type:Date,
+        default:Date.now
+    },
+    referalCode:{
+        type:String,
+    },
+    redeemed:{
+        type:Boolean
+    },
+    redeemedUsers:[{
+       type : Schema.Types.ObjectId,
+       ref:"User"
+    }],
+    searchHistory:[{
+        category: {
+            type:Schema.Types.ObjectId,
+            ref:"Category",
+        },
+        brand:{
+            type:String
+        },
+        searchOn : {
+            type: Date,
+            default: Date.now
+        }
+    }]
 })
 const user = mongoose.model('user',userschema)
 module.exports = user
